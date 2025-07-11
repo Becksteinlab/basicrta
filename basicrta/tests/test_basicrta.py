10c34e97e2cd30044015b3dcbf29020418c6ff5f
Oliver Beckstein <orbeckst@gmail.com>
2025-07-10 18:42:31 -0700

    removed placeholder MDA logo test (#30)
    - fix #29
    - removes all uses of outdated pkg_resources usage
    - removes conftest.py and basicrta.data.files as neither are used in basicrta
    - simplified original data/README.md

--- basicrta/tests/test_basicrta.py
+++ basicrta/tests/test_basicrta.py
@@ -4,7 +4,6 @@ Unit and regression test for the basicrta package.
 
 # Import package, test suite, and other packages as needed
 import basicrta
-import pytest
 import sys
 
 
@@ -13,7 +12,3 @@ def test_basicrta_imported():
     assert "basicrta" in sys.modules
 
 
-def test_mdanalysis_logo_length(mdanalysis_logo_text):
-    """Example test using a fixture defined in conftest.py"""
-    logo_lines = mdanalysis_logo_text.split("\n")
-    assert len(logo_lines) == 46, "Logo file does not have 46 lines!"
